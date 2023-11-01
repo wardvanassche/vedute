0dfdbd9b71c68cc501599abce58d85403a7ce504
Mikael <mjpeijer@gmail.com>
2023-11-01 14:05:27 +0100

    create af + test pagina om alle aangemaakte vedutes te zien

---
+++ php/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+//gegevens database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "vedute";
+
+
+
+//verbind de database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//error melding als het niet werkt
+if ($conn->connect_error) {
+    die("Verbindingsfout: " . $conn->connect_error);
+}
+
+
