5b5258dc0d3dbe84d31c0e994a4061045b74ef08
slmartis <slmartis@outlook.com>
2023-12-14 22:22:02 +0100

    Front end verbeterd

--- pages/adminOverview.php
+++ pages/adminOverview.php
@@ -31,6 +31,8 @@ if ($admin === false){
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
           integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
           crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link href="https://fonts.googleapis.com/css2?family=Afacad&family=Oswald:wght@200;500&display=swap" rel="stylesheet">
+
 </head>
 <header>
     <div class="container">
