e28a252e4d97a5b1fa9c26d9a3f42a8e20e5e2f2
Ward van Asschegit config --global user.email 1060834@hr.nl <1060834@hr.nl>
2023-12-18 15:44:54 +0100

    Carousel slides fixed

--- pages/gallery.php
+++ pages/gallery.php
@@ -91,11 +91,11 @@
                                         aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
-                                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                                <div id="carouselExampleIndicators' . $row['id'] . '" class="carousel slide" data-bs-ride="carousel">
                                     <div class="carousel-indicators">
-                                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                        <button type="button" data-bs-target="#carouselExampleIndicators' . $row['id'] . '" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                        <button type="button" data-bs-target="#carouselExampleIndicators' . $row['id'] . '" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                        <button type="button" data-bs-target="#carouselExampleIndicators' . $row['id'] . '" data-bs-slide-to="2" aria-label="Slide 3"></button>
                                     </div>
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
@@ -108,11 +108,11 @@
                                         <img src="' . $row["photo3"] . '" class="d-block w-100" alt="' . $row["title"] . '">
                                     </div>                                                                     
                                 </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators' . $row['id'] . '" data-bs-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span class="visually-hidden">Previous</span>
                             </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators' . $row['id'] . '" data-bs-slide="next">
                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span class="visually-hidden">Next</span>
                             </button>
