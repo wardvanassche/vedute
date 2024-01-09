aa2113bacdc775212eb34b5c3dfda701d1ad6ca6
wardvanassche <113512344+wardvanassche@users.noreply.github.com>
2024-01-09 14:32:39 +0100

    Update database.php

--- php/database.php
+++ php/database.php
@@ -1,9 +1,15 @@
 <?php
-    //gegevens database
-    $host = "localhost";
-    $database = "vedute";
-    $user = "root";
-    $password = "";
+    // Local DB
+    // $host = "localhost";
+    // $database = "vedute";
+    // $user = "root";
+    // $password = "";
+
+    // Live DB
+    $host = "185.114.157.173";
+    $database = "vlogslif_vedute";
+    $user = "vlogslif_admin";
+    $password = "lol1234";
 
     //verbind de database
     $conn = new mysqli($host, $user, $password, $database);
